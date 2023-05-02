41696e3c2a5954b237e5556447f6074e35d7537f
haniful360 <haniful360@gmail.com>
2023-05-02 12:28:32 +0600

    chefData loadded

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express();
+var cors = require('cors')
 const port = 5000
+
+app.use(cors())
+
+const chefData = require('./data/data.json')
+
+app.get('/chefData', (req, res) => {
+  res.send(chefData);
+})
 
 
 app.get('/', (req, res) => {
